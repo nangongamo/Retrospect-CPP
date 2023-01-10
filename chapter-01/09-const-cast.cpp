a17d286d6f50fffe3a1238ec36c5007957457926
nangongamo <nangongamo@outlook.com>
2023-01-10 11:16:21 +0800

    update

--- chapter-01/09-const-cast.cpp
+++ chapter-01/09-const-cast.cpp
@@ -3,7 +3,16 @@ using namespace std;
 
 int main()
 {
-    
+    system("chcp 65001");
+
+    int num = 100;
+    const int *num2 = &num;
+    int *num3;
+    num3 = const_cast<int *>(num2); // 去除 num2 的常量性赋给 num3
+    cout << &num << "," << num << endl;
+    cout << num2 << "," << *num2 << endl;
+    cout << num3 << "," << *num3 << endl;
+
     system("pause");
     return 0;
 }
